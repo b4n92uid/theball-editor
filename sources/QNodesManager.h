f869652dad92289c2d84611facfb4f1fe720ce35
Beldjouhri Abdelghani <b4n92uid@gmail.com>
2010-12-09 22:52:18 +0100

    Transformation dans le context de l'?diteur

--- sources/QNodesManager.h
+++ sources/QNodesManager.h
@@ -12,64 +12,7 @@
 #include "QVectorBox.h"
 #include "QBrowsEdit.h"
 #include "QVector2Box.h"
-
-class QWater : public QObject
-{
-    Q_OBJECT
-
-public:
-    QWater(QObject* parent, tbe::scene::Water* water);
-
-    void setCurwater(tbe::scene::Water* curwater);
-    tbe::scene::Water* getCurwater() const;
-
-public slots:
-    void SetDeform(double v);
-    void SetSize(const tbe::Vector2f& v);
-    void SetUvRepeat(const tbe::Vector2f& v);
-    void SetSpeed(double v);
-    void SetBlend(double v);
-
-private:
-    tbe::scene::Water* m_curwater;
-};
-
-class QParticles : public QObject
-{
-    Q_OBJECT
-
-public:
-    QParticles(QObject* parent, tbe::scene::ParticlesEmiter* particles);
-
-    void setCurparticles(tbe::scene::ParticlesEmiter* curparticles);
-    tbe::scene::ParticlesEmiter* getCurparticles() const;
-
-public slots:
-    void SetGravity(const tbe::Vector3f& v);
-    void SetFreemove(double v);
-    void SetLifeinit(double v);
-    void SetLifedown(double v);
-    void SetNumber(int v);
-    void SetTexture(const QString& v);
-    void SetContinousMode(int v);
-
-private:
-    tbe::scene::ParticlesEmiter* m_curparticles;
-};
-
-class QMesh : public QObject
-{
-    Q_OBJECT
-
-public:
-    QMesh(QObject* parent, tbe::scene::Mesh* mesh);
-
-    void setCurmesh(tbe::scene::Mesh* curmesh);
-    tbe::scene::Mesh* getCurmesh() const;
-
-private:
-    tbe::scene::Mesh* m_curmesh;
-};
+#include "QNodeBinders.h"
 
 class QNodesManager : public QObject
 {
@@ -80,18 +23,15 @@ public:
     virtual ~QNodesManager();
 
 public slots:
-    void OnNodeNameChanged(const QString& str);
-    void OnNodePosChanged(const tbe::Vector3f& vec);
-    void OnNodeRotChanged(const tbe::Vector3f& vec);
+    void NodeNameChanged(const QString& str);
+    void NodePosChanged(const tbe::Vector3f& vec);
+    void NodeRotChanged(const tbe::Vector3f& vec);
 
-    void OnGuiMeshAlloc();
-    void OnGuiParticleAlloc();
-    void OnGuiWaterAlloc();
+    void MeshNew();
+    void MeshAdd(tbe::scene::Mesh* mesh);
+    void MeshSelect(tbe::scene::Mesh* mesh);
 
 signals:
-    void RegisterMesh(tbe::scene::Mesh*);
-    void RegisterParticles(tbe::scene::ParticlesEmiter*);
-    void RegisterWater(tbe::scene::Water*);
 
 private:
     QLineEdit* m_nodeName;
@@ -138,11 +78,9 @@ private:
     } ParticlesTab;
 
     QTreeView* m_nodeList;
+    QStandardItemModel* m_nodeModel;
 
-    tbe::scene::Node* m_curNode;
-    tbe::scene::Mesh* m_curMesh;
-    tbe::scene::ParticlesEmiter* m_curParticles;
-    tbe::scene::Water* m_curWater;
+    tbe::scene::Node* m_selectedNode;
 
     QWater* m_qwater;
     QMesh* m_qmesh;
--- sources/main.cpp
+++ sources/main.cpp
@@ -10,6 +10,9 @@
 
 int main(int argc, char *argv[])
 {
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
     // initialize resources, if needed
     // Q_INIT_RESOURCE(resfile);
 
@@ -17,6 +20,7 @@ int main(int argc, char *argv[])
 
     MainWindow window;
     window.show();
+    window.OpenFile("D:\\projets\\cpp\\tbengine\\demo\\bin\\medias\\scene.ballscene");
 
     return app.exec();
 }
