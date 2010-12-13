51aae42433e91142ca3d41bc3c675d3ab1465be6
b4n92uid <b4n92uid@localhost>
2010-12-13 22:53:55 +0100

    Fix: Fonctions en lowerCamelCase

--- sources/QVectorBox.cpp
+++ sources/QVectorBox.cpp
@@ -13,28 +13,28 @@ QVectorBox::QVectorBox(QObject* parent, QDoubleSpinBox* x, QDoubleSpinBox* y, QD
     m_y = y;
     m_z = z;
 
-    connect(m_x, SIGNAL(valueChanged(double)), this, SLOT(ComponentValueChanged(double)));
-    connect(m_y, SIGNAL(valueChanged(double)), this, SLOT(ComponentValueChanged(double)));
-    connect(m_z, SIGNAL(valueChanged(double)), this, SLOT(ComponentValueChanged(double)));
+    connect(m_x, SIGNAL(valueChanged(double)), this, SLOT(componentValueChanged(double)));
+    connect(m_y, SIGNAL(valueChanged(double)), this, SLOT(componentValueChanged(double)));
+    connect(m_z, SIGNAL(valueChanged(double)), this, SLOT(componentValueChanged(double)));
 }
 
 QVectorBox::~QVectorBox()
 {
 }
 
-tbe::Vector3f QVectorBox::GetVectorValue()
+tbe::Vector3f QVectorBox::getVectorValue()
 {
     return tbe::Vector3f(m_x->value(), m_y->value(), m_z->value());
 }
 
-void QVectorBox::SetVectorValue(const tbe::Vector3f& value)
+void QVectorBox::setVectorValue(const tbe::Vector3f& value)
 {
     m_x->setValue(value.x);
     m_y->setValue(value.y);
     m_z->setValue(value.z);
 }
 
-void QVectorBox::ComponentValueChanged(double d)
+void QVectorBox::componentValueChanged(double d)
 {
-    emit valueChanged(GetVectorValue());
+    emit valueChanged(getVectorValue());
 }
