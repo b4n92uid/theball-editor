219858aea7aac8027d95059eb8407c8dd4afc0fe
Beldjouhri Abdelghani <b4n92uid@gmail.com>
2011-04-16 21:57:54 +0100

    Imp: installe fr translator; check change on close

--- sources/main.cpp
+++ sources/main.cpp
@@ -10,10 +10,15 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication app(argc, argv);
+
+    QTranslator translator;
+    translator.load("qt_fr", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+
+    app.installTranslator(&translator);
+
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-
-    QApplication app(argc, argv);
 
     QApplication::setOrganizationName("b4n92uid software");
     QApplication::setApplicationName("theBall Editor");
