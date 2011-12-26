2f162abe682cc79c275f91dc5c7d40f324a8a1ee
Beldjouhri Abdelghani <b4n92uid@gmail.com>
2011-12-26 15:43:43 +0100

    Refractor some gui variables and fix notifing changes when no changes

--- sources/QLightInteractor.cpp
+++ sources/QLightInteractor.cpp
@@ -26,36 +26,26 @@ QString QLightInteractor::typeName() const
 void QLightInteractor::setType(int type)
 {
     m_target->setType((tbe::scene::Light::Type)type);
-
-    m_mainwin->notifyChanges(true);
 }
 
 void QLightInteractor::setAmbiant(const tbe::Vector3f& value)
 {
     m_target->setAmbient(tbe::math::vec34(value));
-
-    m_mainwin->notifyChanges(true);
 }
 
 void QLightInteractor::setDiffuse(const tbe::Vector3f& value)
 {
     m_target->setDiffuse(tbe::math::vec34(value));
-
-    m_mainwin->notifyChanges(true);
 }
 
 void QLightInteractor::setSpecular(const tbe::Vector3f& value)
 {
     m_target->setSpecular(tbe::math::vec34(value));
-
-    m_mainwin->notifyChanges(true);
 }
 
 void QLightInteractor::setRadius(double value)
 {
     m_target->setRadius((float)value);
-
-    m_mainwin->notifyChanges(true);
 }
 
 void QLightInteractor::select()
@@ -64,11 +54,11 @@ void QLightInteractor::select()
 
     QNodeInteractor::select();
 
-    connect(m_mainwin->nodesGui.lighTab.type, SIGNAL(activated(int)), this, SLOT(setType(int)));
-    connect(m_mainwin->nodesGui.lighTab.ambiant, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setAmbiant(const tbe::Vector3f&)));
-    connect(m_mainwin->nodesGui.lighTab.diffuse, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setDiffuse(const tbe::Vector3f&)));
-    connect(m_mainwin->nodesGui.lighTab.specular, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setSpecular(const tbe::Vector3f&)));
-    connect(m_mainwin->nodesGui.lighTab.radius, SIGNAL(valueChanged(double)), this, SLOT(setRadius(double)));
+    connect(m_mainwin->nodesGui.light.type, SIGNAL(activated(int)), this, SLOT(setType(int)));
+    connect(m_mainwin->nodesGui.light.ambiant, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setAmbiant(const tbe::Vector3f&)));
+    connect(m_mainwin->nodesGui.light.diffuse, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setDiffuse(const tbe::Vector3f&)));
+    connect(m_mainwin->nodesGui.light.specular, SIGNAL(valueChanged(const tbe::Vector3f&)), this, SLOT(setSpecular(const tbe::Vector3f&)));
+    connect(m_mainwin->nodesGui.light.radius, SIGNAL(valueChanged(double)), this, SLOT(setRadius(double)));
 
     update();
 
@@ -79,11 +69,11 @@ void QLightInteractor::deselect()
 {
     QNodeInteractor::deselect();
 
-    disconnect(m_mainwin->nodesGui.lighTab.type, SIGNAL(activated(int)), 0, 0);
-    disconnect(m_mainwin->nodesGui.lighTab.ambiant, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
-    disconnect(m_mainwin->nodesGui.lighTab.diffuse, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
-    disconnect(m_mainwin->nodesGui.lighTab.specular, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
-    disconnect(m_mainwin->nodesGui.lighTab.radius, SIGNAL(valueChanged(double)), 0, 0);
+    disconnect(m_mainwin->nodesGui.light.type, SIGNAL(activated(int)), 0, 0);
+    disconnect(m_mainwin->nodesGui.light.ambiant, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
+    disconnect(m_mainwin->nodesGui.light.diffuse, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
+    disconnect(m_mainwin->nodesGui.light.specular, SIGNAL(valueChanged(const tbe::Vector3f&)), 0, 0);
+    disconnect(m_mainwin->nodesGui.light.radius, SIGNAL(valueChanged(double)), 0, 0);
 }
 
 void QLightInteractor::update()
@@ -91,17 +81,17 @@ void QLightInteractor::update()
     QNodeInteractor::update();
 
     QSignalBlocker blocker;
-    blocker << m_mainwin->nodesGui.lighTab.type << m_mainwin->nodesGui.lighTab.ambiant
-            << m_mainwin->nodesGui.lighTab.diffuse << m_mainwin->nodesGui.lighTab.specular
-            << m_mainwin->nodesGui.lighTab.radius;
+    blocker << m_mainwin->nodesGui.light.type << m_mainwin->nodesGui.light.ambiant
+            << m_mainwin->nodesGui.light.diffuse << m_mainwin->nodesGui.light.specular
+            << m_mainwin->nodesGui.light.radius;
 
     blocker.block();
 
-    m_mainwin->nodesGui.lighTab.type->setCurrentIndex((int)m_target->getType());
-    m_mainwin->nodesGui.lighTab.ambiant->setValue(tbe::math::vec43(m_target->getAmbient()));
-    m_mainwin->nodesGui.lighTab.diffuse->setValue(tbe::math::vec43(m_target->getDiffuse()));
-    m_mainwin->nodesGui.lighTab.specular->setValue(tbe::math::vec43(m_target->getSpecular()));
-    m_mainwin->nodesGui.lighTab.radius->setValue(m_target->getRadius());
+    m_mainwin->nodesGui.light.type->setCurrentIndex((int)m_target->getType());
+    m_mainwin->nodesGui.light.ambiant->setValue(tbe::math::vec43(m_target->getAmbient()));
+    m_mainwin->nodesGui.light.diffuse->setValue(tbe::math::vec43(m_target->getDiffuse()));
+    m_mainwin->nodesGui.light.specular->setValue(tbe::math::vec43(m_target->getSpecular()));
+    m_mainwin->nodesGui.light.radius->setValue(m_target->getRadius());
 
     blocker.unblock();
 
